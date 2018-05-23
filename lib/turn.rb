3227db1454031b4cdc4093d2f7a52ea9e32af158
Nathan Hill <nateohill@gmail.com>
2018-05-23 16:57:47 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,7 +30,7 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   user_input = input_to_index(user_input)
-  while input_to_index(user_input) === -1
+  while user_input === -1
     puts "Please enter 1-9"
     user_input = gets.strip
     user_input = input_to_index(user_input)
